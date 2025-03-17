c6a8b5e1e855f68f6ce3b9419e8c3f391e7ef9b1
Fabius01k <pashamuruskinn@gmail.com>
2025-03-17 23:44:47 +0300

    add env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const allowedOrigins = process.env.CORS_ORIGINS.split(',')
+      .map(origin => origin.trim());
+  console.log(allowedOrigins, 'allowedOrigins');
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(process.env.APP_PORT);
 }
+
+console.log('process.env.APP_PORT - ', process.env.APP_PORT);
 bootstrap();
